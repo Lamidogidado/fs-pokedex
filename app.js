4515e1633518de6dfc7a9d702fc30f7e52042361
muhammad lamido <lamido361@gmail.com>
2026-03-30 19:26:33 +0100

    fix: restore health check and move to version 3

--- app.js
+++ app.js
@@ -8,14 +8,12 @@ app.use(express.static('dist'))
 
 // Add the version endpoint here
 app.get('/version', (req, res) => {
-  res.send('2') // change this string to '2', '3' etc. to verify new deploys
+  res.send('3') // change this string to '2', '3' etc. to verify new deploys
 })
 
 // Add a health check (good practice for CI/CD)
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-unused-vars
-  const _unused = { req, res }
-  throw new Error('intentional crash')
+  res.send('ok')
 })
 const start = async () => {
   app.listen(PORT, () => {
